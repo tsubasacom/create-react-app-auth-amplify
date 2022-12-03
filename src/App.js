7195e694a7e852c60f9cb60e22ce95abccc1e409
Tsubasa <t.nakao@nanisuru.co.jp>
2022-12-03 16:51:14 +0900

    認証追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'aws-amplify/ui-react/style.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <h1>Sample App</h1>
-        <p>これは、サンプルで作成したアプリです。</p>
-      </div>
-    );
+
+function App(){
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <AmplifySignOut />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <h1>Sample App</h1>
+          <p>これは、サンプルで作成したアプリです。</p>
+        </div>
+      );
+    }
   }
 }
 
